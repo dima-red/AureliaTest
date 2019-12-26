c82ed398d4611435e3e120ce0880691a922d1ddf
Dmitry Goman <DmitryGoman@coherentsolutions.com>
2019-12-26 16:47:30 +0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
+import { throwStatement } from "../../../../AppData/Local/Microsoft/TypeScript/3.7/node_modules/@babel/types/lib/index";
+
 export class App {
-  message = 'Hello World!';
-}
+  constructor() {
+    this.heading = 'Todos';
+    this.todos = [];
+    this.todoDescription = '';
+  };
+
+  addTodo() {
+    if (this.todoDescription) {
+      this.todos.push({
+        description: this.todoDescription,
+        done: false
+      });
+      this.todoDescription = '';
+    };
+  };
+
+  removeTodo(todo, ev) {
+    let index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    };
+
+    console.log(ev);
+  };
+};
